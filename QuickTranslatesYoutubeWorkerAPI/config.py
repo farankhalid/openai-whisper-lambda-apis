82d98180ea7aec09ee63e7a568008f0582a5a41f
Faran Khalid <68998531+farankhalid@users.noreply.github.com>
2024-02-21 15:58:25 +0500

    Update config.py

--- QuickTranslatesYoutubeWorkerAPI/config.py
+++ QuickTranslatesYoutubeWorkerAPI/config.py
@@ -10,10 +10,10 @@ def get_parameter(parameter_name):
 
 
 # AWS Parameter Store parameter names
-AWS_REGION_NAME = "/quick-translates/AWS_REGION_NAME"
-PUSHER_APP_ID = "/quick-translates/PUSHER_APP_ID"
-PUSHER_KEY = "/quick-translates/PUSHER_KEY"
-PUSHER_SECRET = "/quick-translates/PUSHER_SECRET"
-PUSHER_CLUSTER = "/quick-translates/PUSHER_CLUSTER"
-WORKER_QUEUE_URL = "/quick-translates/WORKER_QUEUE_URL"
-YOUTUBE_QUEUE_URL = "/quick-translates/YOUTUBE_QUEUE_URL"
+AWS_REGION_NAME = "/openai-whisper/AWS_REGION_NAME"
+PUSHER_APP_ID = "/openai-whisper/PUSHER_APP_ID"
+PUSHER_KEY = "/openai-whisper/PUSHER_KEY"
+PUSHER_SECRET = "/openai-whisper/PUSHER_SECRET"
+PUSHER_CLUSTER = "/openai-whisper/PUSHER_CLUSTER"
+WORKER_QUEUE_URL = "/openai-whisper/WORKER_QUEUE_URL"
+YOUTUBE_QUEUE_URL = "/openai-whisper/YOUTUBE_QUEUE_URL"
